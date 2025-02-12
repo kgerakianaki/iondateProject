b43548c39b0d290e6f29966ede3050a2aa71b997
kgerakianaki <mkg@entersoft.gr>
2025-02-12 12:31:26 +0200

    Ion date time

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { IonDatetime } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+  standalone: false
 })
 export class HomePage {
+  // Variable to hold selected date
+  selectedDate: string = '';
+  minDate: string = '';
 
   constructor() {}
+  
+  ngAfterViewInit() {
+    this.minDate = new Date().toISOString().split('T')[0];
+  }
+
+  // Function that gets called when the date changes
+  onDateChange(event: any) {
+    // Log the selected date to the console
+    console.log('Selected Date:', event.detail.value);
+  }
+
+
 
 }
